c595762094422863d46cb61ca81c585bbb574ae7
Thomas Martin <tmartin@reciprocityindustries.com>
2021-11-07 18:23:18 -0700

    first run at genre tabs, can't get hilighting to work with scrollspy

--- components/genres.js
+++ components/genres.js
@@ -1,22 +1,34 @@
 import Genre from './genre';
-import { useState } from 'react';
+// import bootstrap from 'bootstrap'
 
 const GENRES = ['rap', 'pop', 'edm', 'r&b', 'rock', 'latin'];
 
 export default function Genres() {
-  const [activeGenrePill, setActiveGenrePill] = useState(0)
+  // var firstScrollSpyEl = document.querySelector('[data-bs-spy="scroll"]')
+  // firstScrollSpyEl.addEventListener('activate.bs.scrollspy', function () {
+  //   console.log('scrollspy worked')
+  // })
+  // var dataSpyList = [].slice.call(document.querySelectorAll('[data-bs-spy="scroll"]'))
+  // dataSpyList.forEach(function (dataSpyEl) {
+  //   bootstrap.ScrollSpy.getInstance(dataSpyEl)
+  //     .refresh()
+  // })
+  // document && document.addEventListener('scroll', ())
   return (
-    <>
-      <ul className="nav nav-pills nav-fill">
-        {GENRES.map((genre, genreIndex) => (
-          <li className="nav-item" key={genre}>
-            <a className="nav-link text-capitalize py-5" aria-current="page" aria-selected={genreIndex === activeGenrePill} href={"#" + genre}>{genre}</a>
-          </li>
-        ))}
-      </ul>
+    <div data-bs-spy="scroll" data-offset="0" tabIndex="0" data-bs-target="#genre-selector">
+      <nav className="nav m-5 shadow-sm sticky-top navbar-light bg-light nav-pills nav-fill px-3" id="genre-selector">
+      {/* <nav className="navbar navbar-light bg-light px-3" id="genre-selector"> */}
+        {/* <ul className="nav nav-pills"> */}
+          {GENRES.map((genre, genreIndex) => (
+              <li className="nav-item" key={genreIndex}>
+                <a className="nav-link fs-5 fw-bold text-capitalize py-3" href={"#" + genre}>{genre}</a>
+              </li>
+          ))}
+        {/* </ul> */}
+      </nav>
       {GENRES.map(genre => (
-        <Genre key={genre} genre={genre} />
+        <Genre key={genre} genre={genre}/>
       ))}
-    </>
+    </div>
   );
 }
